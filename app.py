6335abebd984aeca9ff715fc128dad9d58f149f0
Stephanie Simms <stephanie.simms@gmail.com>
2019-06-19 16:11:12 -0700

    skeleton files for app

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'super-secret'
+
+debug = DebugToolbarExtension(app)
+
