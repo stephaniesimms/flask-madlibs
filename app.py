b8fef83f7487a9a4985a8e99439a3f64c04a28cd
Stephanie Simms <stephanie.simms@gmail.com>
2019-06-19 17:24:17 -0700

    basic solution

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from stories import Story, story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret'
 
 debug = DebugToolbarExtension(app)
 
+@app.route('/')
+def form_template():
+    """Return homepage with form template"""
+    prompts = story.prompts
+    return render_template("form.html", prompts=prompts)
+
+@app.route('/story')
+def create_story():
+    """Show rendered story""" 
+
+    story_text = story.generate(request.args)
+    print(story_text)
+
+    return render_template("story.html", story_text=story_text)
